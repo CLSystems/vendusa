6db88e9370835db8d05ba0ce7aa8a7ef22acaad3
Jeroen Guyt <jeroen@clsystems.nl>
2019-01-18 09:49:55 +0100

    JER-1 Switch to OC system

--- app/middleware.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Application middleware
-
-// e.g: $app->add(new \Slim\Csrf\Guard);
-
-$app->add(function ($request, $response, $next) {
-    $this->view->offsetSet('flash', $this->flash);
-    return $next($request, $response);
-});
--- app/route.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-	Routes
-	controller needs to be registered in dependency.php
-*/
-
-$app->get('/', 'App\Controllers\HomeController:dispatch')->setName('homepage');
-
-$app->get('/users', 'App\Controllers\UserController:dispatch')->setName('userpage');
